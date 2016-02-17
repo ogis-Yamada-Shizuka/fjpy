278159a151b4d1c07826bf832213226566f2e25f
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-02-17 18:20:24 +0900

    ScheduleStatus.ob_xxx メソッドは ID を期待していたので修正

--- app/models/schedule_status.rb
+++ app/models/schedule_status.rb
@@ -2,7 +2,7 @@ class ScheduleStatus < ActiveRecord::Base
   class << self
     Constants::ScheduleStatus.constants.each_with_index do |id, i|
       define_method "of_#{id.to_s.sub(/ID_/, '').downcase}" do
-        Constants::ScheduleStatus.constants[i]
+        const_get("Constants::ScheduleStatus::#{Constants::ScheduleStatus.constants[i].to_s}")
       end
     end
   end
