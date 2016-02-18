54d9f2374d79ae50b3a4afd7e63b6dcc88b73a78
silica_SiO2 <yamada.sn.shizuka@gmail.com>
2016-02-18 02:47:50 +0000

    detele TODO comment.

--- app/controllers/inspection_results_controller.rb
+++ app/controllers/inspection_results_controller.rb
@@ -33,8 +33,6 @@ class InspectionResultsController < ApplicationController
   def create
     @inspection_result = InspectionResult.new(inspection_result_params)
 
-
-    # TODO: ここで落ちる kilogy からのバグがある。(inspection_schedule_id を hidden で入れる？)
     inspection = InspectionSchedule.where(id: params[:inspection_result][:inspection_schedule_id]).first
 
     inspection.start_inspection # 点検開始(ステータスを点検実施中に変える)
