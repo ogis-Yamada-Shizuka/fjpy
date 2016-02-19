ae28e93cc7f701ad047deb50da7ed9ddcddfe3ca
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-02-19 18:13:25 +0900

    ApplicationHelper のメソッド記述順を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 module ApplicationHelper
   include Common
 
+  # 点検予定一覧画面に進捗状況のクエリパラメーターを与えたパスを生成
+  Constants::ScheduleStatus.constants.each do |id|
+    schedule_status_name = id.to_s.sub(/ID_/, '').downcase
+    define_method "inspection_schedules_path_of_#{schedule_status_name}" do
+      inspection_schedules_path + "/?[schedule_status_id_eq]=#{ScheduleStatus.send("of_#{schedule_status_name}")}"
+    end
+  end
+
   # 点検依頼一覧(直近のみ)
   def requested_inspection_schedules_path
     inspection_schedules_path + "/?q[schedule_status_id_eq]=#{ScheduleStatus.of_requested}&" + latest_parameter
@@ -29,13 +37,5 @@ module ApplicationHelper
   # 直近(2ヶ月後以前)
   def latest_parameter
     "q[target_yearmonth_date_lteq]=#{Date.parse(current_date) >> 2}"
-  end
-
-  # 点検予定一覧画面に進捗状況のクエリパラメーターを与えたパスを生成
-  Constants::ScheduleStatus.constants.each do |id|
-    schedule_status_name = id.to_s.sub(/ID_/, '').downcase
-    define_method "inspection_schedules_path_of_#{schedule_status_name}" do
-      inspection_schedules_path + "/?[schedule_status_id_eq]=#{ScheduleStatus.send("of_#{schedule_status_name}")}"
-    end
   end
 end
