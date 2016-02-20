267de513a8462582f75d83cdb02db4e90007c717
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-02-20 23:04:24 +0900

    点検予定一覧編集フォームを最新の属性に修正

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  $(".monthpicker").datepicker({
+  $(".date.datepicker").datepicker({
+    format: 'yyyy年mm月dd日',
+    language: 'ja'
+  });
+
+  $(".month.datepicker").datepicker({
     format: 'yyyy年mm月',
     language: 'ja',
     minViewMode : 'months'
