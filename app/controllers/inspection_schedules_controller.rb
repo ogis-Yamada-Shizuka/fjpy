bc7eac32a44a6d8baf014543c9b4051e63f10480
silica_SiO2 <yamada.sn.shizuka@gmail.com>
2016-03-04 05:46:54 +0000

    move setup googlemap-marker to model(InspectionResult)

--- app/controllers/inspection_schedules_controller.rb
+++ app/controllers/inspection_schedules_controller.rb
@@ -73,12 +73,7 @@ class InspectionSchedulesController < ApplicationController
   end
 
   def close_inspection
-    @marker = Gmaps4rails.build_markers(@inspection_schedule.result) do |inspection_result, marker|
-      marker.lat inspection_result.latitude
-      marker.lng inspection_result.longitude
-      marker.infowindow inspection_result.updated_at.to_s
-      marker.json(title: inspection_result.user_id.to_s)
-    end
+    @marker = @inspection_schedule.result.setup_marker
   end
 
   # GET /inspection_schedules/new
