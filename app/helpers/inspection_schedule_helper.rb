8484d0be7458f9246e9d0c91b8097ac6bf0a3f29
silica_SiO2 <yamada.sn.shizuka@gmail.com>
2016-03-09 06:37:05 +0000

    replace datetimeformatter helper->i18n.l and bugfix(make @marker only exist inspection_result

--- app/helpers/inspection_schedule_helper.rb
+++ app/helpers/inspection_schedule_helper.rb
@@ -92,8 +92,4 @@ module InspectionScheduleHelper
   def date_value(date)
     date.strftime("%Y年%m月%d日") if date.present?
   end
-
-  def datetime_value(datetime)
-    datetime.strftime("%Y年%m月%d日%H時%M分") if datetime.present?
-  end
 end
