5577ca83095a6f95e6d0fa3cc32c718d201a9e7c
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-03-10 11:56:02 +0900

    メソッド名変更

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,9 +1,9 @@
 module I18n
   class << self
-    alias_method :safe_localize, :localize
+    alias_method :original_localize, :localize
 
     def localize(object, options = {})
-      object.present? ? safe_localize(object, options) : ''
+      object.present? ? original_localize(object, options) : ''
     end
   end
 end
